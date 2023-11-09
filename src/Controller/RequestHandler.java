e3686049054dcb9527d105666a8fe391a7b56ab3
viktorpolak98 <vigge.polak98@gmail.com>
2023-11-09 18:59:35 +0100

    Started implementing test
    Also added .start() to request listener thread

--- src/Controller/RequestHandler.java
+++ src/Controller/RequestHandler.java
@@ -36,7 +36,7 @@ public class RequestHandler {
                     connected = false;
                 }
             }
-        });
+        }).start();
 
     }
 }
--- src/Tests/TestDBCalls.java
+++ src/Tests/TestDBCalls.java
@@ -1,20 +1,31 @@
 package Tests;
 
 import Controller.CallerController;
+import Controller.DbCaller;
+import Model.DbCalls;
+import Model.Request;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+
 public class TestDBCalls {
-    private static CallerController callerController;
+    private static DbCaller dbCaller;
+    private static String userName = "new_user";
+    private static String password = "password";
 
     @BeforeAll
     public static void setUp(){
-        callerController = new CallerController(System.getenv("mockDbUrl"));
+        dbCaller = new DbCaller(System.getenv("mockDbUrl"));
     }
 
     @Test
     public void testAllCalls(){
         //TODO: implement
+        dbCaller.execute(new Request(userName, password, DbCalls.createUser));
+        dbCaller.execute(new Request(userName, password, DbCalls.authenticateUser));
+        dbCaller.execute(new Request(userName, "new_" + password, DbCalls.updatePassword));
+        dbCaller.execute(new Request(userName, DbCalls.getUser));
+        dbCaller.execute(new Request(userName, DbCalls.removeUser));
     }
 
     @Test
