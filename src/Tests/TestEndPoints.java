2fa3fd90df6b8511f59ce0b180eb3d0e49d18984
viktorpolak98 <vigge.polak98@gmail.com>
2024-04-09 19:13:49 +0200

    Added testdata and finished first endpoint test

--- src/Tests/TestEndPoints.java
+++ src/Tests/TestEndPoints.java
@@ -1,10 +1,12 @@
 package Tests;
 
 import Controller.CallerController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -23,21 +25,45 @@ public class TestEndPoints {
     @Test
     public void testCreateUser(){
         HttpURLConnection connection;
-        URL url = createUrl("/create-user");
+
+        //replace with Testdata.Testdata.xml later.
+        String username = "user1";
+        String password = "password";
+        String endpoint = String.format("/create-user/%s/%s", username, password);
+        URL url = createUrl(endpoint);
 
         Assumptions.assumeFalse(url == null);
         Assumptions.assumeFalse(url.getPath().isBlank());
 
+        connection = createConnection(url, "POST");
+        Assumptions.assumeFalse(connection == null);
+
+        String response = "";
+
         try {
-
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-
-
-            //TODO finish implementing
+            connection.connect();
+            response = connection.getResponseMessage();
+            connection.disconnect();
         } catch (IOException e){
             e.printStackTrace();
         }
+
+        Assertions.assertFalse(response.isBlank(), "response was blank");
+        Assertions.assertEquals("HTTP/1.0 200 OK", response); //TODO: Replace with actual expected response
+    }
+
+    private HttpURLConnection createConnection(URL url, String requestMethod){
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(requestMethod);
+            connection.setRequestProperty("Content-Type", "text/plain");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return connection;
     }
 
     private URL createUrl(String endpoint){
