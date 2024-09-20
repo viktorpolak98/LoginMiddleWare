5900ac45ac1df137dc52f400932667730a0f6738
viktorpolak98 <vigge.polak98@gmail.com>
2024-09-20 17:31:45 +0200

    Added SPInvalidateKey

---
+++ src/main/java/Database/StoredProcedures/SPInvalidateKey.sql
@@ -0,0 +1,21 @@
+USE [UserDb]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE procedure [dbo].[InvalidateKey] @APIKey nvarchar(195)
+As
+    DECLARE @yesterday date = DATEADD(day, -1, CAST(GETDATE() AS date));
+	DECLARE @hashedKey nvarchar(195) = HASHBYTES('SHA2_512', @APIKey);
+
+    UPDATE APIKeys
+    SET ValidTo = @yesterday
+    WHERE APIKey = @hashedKey;
+
+GO
