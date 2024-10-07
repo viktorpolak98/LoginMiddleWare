3377483e6e8a73672c95a7d46009ef083bb6b7f9
viktorpolak98 <vigge.polak98@gmail.com>
2024-10-07 17:51:29 +0200

    Unit tests for APIKey authentication

--- src/test/java/TestDBAuthenticationCalls.java
+++ src/test/java/TestDBAuthenticationCalls.java
@@ -1,13 +1,21 @@
 import Controller.Database.DbAPIRequestCaller;
+import Model.DbAPIKeyCalls;
+import Model.DbAPIKeyRequest;
+import Model.Status;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.sql.*;
 
 public class TestDBAuthenticationCalls {
 
     private static DbAPIRequestCaller dbAPIRequestCaller;
-    private static final String emailAddress = "example@example.com";
-    private static final String APIKey = "KeyExample123";
+    private final String emailAddress = "example@example.com";
+    private final String emailAddress2 = "example1@example1.com";
+    private final String APIKey = "KeyExample1";
+    private final String APIKey2 = "KeyExample2";
 
     @BeforeAll
     public static void setUp() {
@@ -15,6 +23,43 @@ public class TestDBAuthenticationCalls {
                 System.getenv("MockDbUser"),
                 System.getenv("MockDbUserPassword"));
         cleanUpDatabase();
+    }
+
+    @Test
+    public void testAuthenticateAPIKey() {
+        Assumptions.assumeTrue(setUpUserAndKey());
+
+        Status status = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.AuthenticateKey, emailAddress, APIKey));
+        Assertions.assertEquals(Status.OK, status);
+
+        status = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.AuthenticateKey, emailAddress, " "));
+        Assertions.assertEquals(Status.BAD_REQUEST, status);
+
+        status = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.AuthenticateKey, emailAddress, ""));
+        Assertions.assertEquals(Status.BAD_REQUEST, status);
+
+        status = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.AuthenticateKey, emailAddress, "notAKey"));
+        Assertions.assertEquals(Status.UNAUTHORIZED, status);
+
+        status = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.AuthenticateKey, "notAUser", APIKey));
+        Assertions.assertEquals(Status.OK, status);
+
+        status = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.AuthenticateKey, emailAddress, APIKey2));
+        Assertions.assertEquals(Status.UNAUTHORIZED, status);
+    }
+
+    private boolean setUpUserAndKey() {
+        Status user = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.CreateUser, emailAddress));
+        Status key = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.CreateKey, emailAddress, APIKey));
+
+        boolean user1Status = key == Status.OK && user == Status.OK;
+
+        user = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.CreateUser, emailAddress2));
+        key =  dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.CreateKey, emailAddress2, APIKey2));
+
+        boolean user2Status = key == Status.OK && user == Status.OK;
+
+        return  user1Status && user2Status;
     }
 
     private static void cleanUpDatabase() {
