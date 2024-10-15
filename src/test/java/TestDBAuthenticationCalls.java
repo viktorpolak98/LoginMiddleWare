a592bd1df13ffbb311c73285e919ecf2fa4c0f71
viktorpolak98 <vigge.polak98@gmail.com>
2024-10-15 17:27:58 +0200

    Add test for key invalidation

--- src/test/java/TestDBAuthenticationCalls.java
+++ src/test/java/TestDBAuthenticationCalls.java
@@ -27,7 +27,7 @@ public class TestDBAuthenticationCalls {
 
     @Test
     public void testAuthenticateAPIKey() {
-        Assumptions.assumeTrue(setUpUsersAndKeys());
+        Assumptions.assumeTrue(setupUsersAndKeys());
 
         Assertions.assertEquals(Status.OK, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.AuthenticateKey, emailAddress, APIKey)));
 
@@ -66,7 +66,7 @@ public class TestDBAuthenticationCalls {
         Assertions.assertEquals(Status.OK, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.CreateKey, emailAddress, APIKey)));
 
         Assertions.assertEquals(Status.OK, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.CreateKey,
-                emailAddress, APIKey2, new Date(System.currentTimeMillis() + 100_000))));
+                emailAddress, APIKey2, new Date(System.currentTimeMillis()))));
 
         Assertions.assertEquals(Status.BAD_REQUEST, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.CreateKey, emailAddress, APIKey)));
 
@@ -87,7 +87,30 @@ public class TestDBAuthenticationCalls {
         Assertions.assertEquals(Status.BAD_REQUEST, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.CreateKey, null, APIKey)));
     }
 
-    private boolean setUpUsersAndKeys() {
+    @Test
+    public void testInvalidateAPIKey() {
+        Assumptions.assumeTrue(setupUsersAndKeys());
+
+        Assertions.assertEquals(Status.OK, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.InvalidateKey, emailAddress, APIKey)));
+
+        Assertions.assertEquals(Status.OK, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.InvalidateKey,
+                emailAddress, APIKey))); //Should be possible to invalidate already invalid key
+
+        Assertions.assertEquals(Status.NOT_FOUND, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.InvalidateKey, emailAddress, APIKey2)));
+
+        Assertions.assertEquals(Status.BAD_REQUEST, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.InvalidateKey, "", "")));
+
+        Assertions.assertEquals(Status.BAD_REQUEST, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.InvalidateKey, " ", " ")));
+
+        Assertions.assertEquals(Status.BAD_REQUEST, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.InvalidateKey, null, null)));
+
+        Assertions.assertEquals(Status.BAD_REQUEST, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.InvalidateKey, emailAddress, null)));
+
+        Assertions.assertEquals(Status.BAD_REQUEST, dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.InvalidateKey, null, APIKey)));
+
+    }
+
+    private boolean setupUsersAndKeys() {
         Status user = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.CreateUser, emailAddress));
         Status key = dbAPIRequestCaller.execute(new DbAPIKeyRequest(DbAPIKeyCalls.CreateKey, emailAddress, APIKey));
 
