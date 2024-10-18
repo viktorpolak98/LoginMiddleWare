78bf76a48e312ed7f8f1edb9e76a44823ba124bb
viktorpolak98 <vigge.polak98@gmail.com>
2024-10-18 19:26:54 +0200

    Refactor to include /v1 in routes

--- src/main/java/Controller/CallerController.java
+++ src/main/java/Controller/CallerController.java
@@ -34,15 +34,15 @@ public class CallerController {
     public void initRoutes(Javalin app){
         app.before(this::beforeRequest);
 
-        app.patch("/update-password/", this::updatePassword);
+        app.patch("/v1/update-password/", this::updatePassword);
 
-        app.delete("/remove-user/", this::removeUser);
+        app.delete("/v1/remove-user/", this::removeUser);
 
-        app.post("/create-user/", this::createUser);
+        app.post("/v1/create-user/", this::createUser);
 
-        app.get("/authenticate/", this::authenticate);
+        app.get("/v1/authenticate/", this::authenticate);
 
-        app.get("/user/", this::user);
+        app.get("/v1/user/", this::user);
 
 
     }
