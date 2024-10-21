ecaa4a6eca54c5a19df0d58bbc6ab74738eaf88b
viktorpolak98 <vigge.polak98@gmail.com>
2024-10-21 20:54:38 +0200

    Add parameters to request urls and fix test

--- src/test/java/TestGetUserEndpoint.java
+++ src/test/java/TestGetUserEndpoint.java
@@ -54,18 +54,13 @@ public class TestGetUserEndpoint extends EndpointParent {
         User user = getUsers().get(0);
 
         String response = getCalls().createUser(user.getUsername(), user.getPassword());
-        Assumptions.assumeTrue(response.equals(getHTTP_200()));
-
-        response = getCalls().getUser(user.getUsername());
-        Assumptions.assumeTrue(response.equals(getHTTP_200()));
+        Assumptions.assumeTrue(response.equals(getHTTP_201()));
 
         response = getCalls().removeUser(user.getUsername());
         Assumptions.assumeTrue(response.equals(getHTTP_200()));
 
         response = getCalls().createUser(user.getUsername(), user.getPassword());
-        Assumptions.assumeTrue(response.equals(getHTTP_200()));
+        Assumptions.assumeTrue(response.equals(getHTTP_201()));
 
-        response = getCalls().getUser(user.getUsername());
-        Assertions.assertEquals(getHTTP_200(), response);
     }
 }
